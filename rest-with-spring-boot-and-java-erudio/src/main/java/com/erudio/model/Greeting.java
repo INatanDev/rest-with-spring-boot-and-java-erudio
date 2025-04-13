1b3772b7542862506ad9f1e68583085d9900b004
inatandev <natan.ferreira21@hotmail.com>
2025-04-12 21:26:33 -0300

    First Steps in Java with Spring Boot 3.4.0

---
+++ rest-with-spring-boot-and-java-erudio/src/main/java/com/erudio/model/Greeting.java
@@ -0,0 +1,4 @@
+package com.erudio.model;
+
+public record Greeting(Long id, String content) {
+}
---
+++ rest-with-spring-boot-and-java-erudio/src/test/java/com/erudio/StartupTests.java
@@ -0,0 +1,14 @@
+package com.erudio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Configuration;
+
+@SpringBootTest
+class StartupTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
